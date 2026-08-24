4a91d4bcc53a4925a747591835ff42701c6ab54a
Guilherme Reis <guireis7373@gmail.com>
2026-08-24 08:58:37 -0300

    Exercicio 1 - Repetição

--- 03 - Repetição/01 - Exercicio/script.js
+++ 03 - Repetição/01 - Exercicio/script.js
@@ -1,1 +1,44 @@
-n
+/*var inicio = 0;
+
+var final = 10;
+
+var passo = 2;
+
+for (var inicio = 0; inicio <= final; inicio += passo){
+    console.log(inicio)
+}*/
+
+
+function executar() {
+    var campoIni = document.getElementById('txtini')
+    var campoFim = document.getElementById('txtfim')
+    var campoPas = document.getElementById('txtpas')
+    var res = document.getElementById('res')
+
+    var ini = Number(campoIni.value)
+    var fim = Number(campoFim.value)
+    var pas = Number(campoPas.value)
+
+    if (campoIni.value === '' || campoFim.value === '') {
+        res.innerHTML = '[ERROR] Verificar se algum campo está vazio'
+    } else {
+        if (campoPas.value === '' || pas === 0) {
+            alert('Campo "Passo" inválido, será considerado como 1')
+            pas = 1
+        }
+
+        if (ini < fim) {
+            res.innerHTML = ''
+
+            for (ini; ini <= fim; ini += pas) {
+                res.innerHTML += ini + ' '
+            }
+        } else {
+            res.innerHTML = ''
+
+            for (ini; ini >= fim; ini -= pas) {
+                res.innerHTML += ini + ' '
+            }
+        }
+    }
+}
